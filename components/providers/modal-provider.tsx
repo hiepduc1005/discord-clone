93da624816baa04fb29f70a277cdbffea1a7224b
hiepduc1005 <hiepngo1524@gmail.com>
2024-12-26 15:38:53 +0700

    update role and kick feature

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { CreateServerModal } from "../modals/create-server-modal"
 import { InviteModal } from "../modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
+import { MembersModal } from "../modals/members-modal";
 
 export const ModalProvider = () => {
     const [isMounted,setIsMounted]  = useState(false);
@@ -19,7 +20,8 @@ export const ModalProvider = () => {
     return(
         <>
             <CreateServerModal />
-            <InviteModal />
+            <InviteModal />\
+            <MembersModal></MembersModal>
             <EditServerModal />
         </>
     )
