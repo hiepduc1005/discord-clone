f48f95a6faf0c993f57b4bb78ee17d9950fbcaa1
hiepduc1005 <hiepngo1524@gmail.com>
2024-12-28 22:53:39 +0700

    update

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -12,34 +12,34 @@ const ioHandler = async (req: NextApiRequest, res: NextApiResponseServerIo) => {
     try {
       console.log('Socket.io handler is called'); // Log when the handler is hit
 
-    //     // Kiểm tra xem server có socket.io chưa
-    //     if (!res.socket.server.io) {
-    //         const path = '/api/socket/io';
-    //         console.log(path)
-    //         const httpServer: NetServer = res.socket.server as any;
+        // Kiểm tra xem server có socket.io chưa
+        if (!res.socket.server.io) {
+            const path = '/api/socket/io';
+            console.log(path)
+            const httpServer: NetServer = res.socket.server as any;
             
-    //         // Khởi tạo Socket.io server
-    //         const io = new ServerIO(httpServer, {
-    //             path: path,
-    //             addTrailingSlash: false,
+            // Khởi tạo Socket.io server
+            const io = new ServerIO(httpServer, {
+                path: path,
+                addTrailingSlash: false,
                 
-    //             transports: ['polling', 'websocket']
-    //         });
+                transports: ['polling', 'websocket']
+            });
             
-    //         console.log(io)
-    //         res.socket.server.io = io; // Gán socket.io vào server
+            console.log(io)
+            res.socket.server.io = io; // Gán socket.io vào server
 
-    //         io.on('error', (err) => {
-    //             console.log('Socket.IO error:', err.message, err.stack);
-    //           });
-    //     }
+            io.on('error', (err) => {
+                console.log('Socket.IO error:', err.message, err.stack);
+              });
+        }
 
         res.end(); // Kết thúc yêu cầu
 
     } catch (error) {
         // In lỗi vào console nếu có lỗi xảy ra
         console.log('Error initializing socket.io:', error);
-        res.status(500).send({'Internal Server Error':error });
+        res.status(500).send({'Internal Server Error':res });
     }
 };
 
