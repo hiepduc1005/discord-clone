fe8e8726621577981e3825264f1801dbf8359a81
hiepduc1005 <hiepngo1524@gmail.com>
2024-12-28 21:52:33 +0700

    update

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -3,39 +3,9 @@ import { Server as ServerIO } from "socket.io";
 import { NextApiResponseServerIo } from "@/type";
 import { NextApiRequest } from 'next';
 
-export const config = {
-    api: {
-        bodyParser: false, // Disable body parsing for Socket.io requests
-    },
-};
-console.log('Socket.io init'); // Log when the handler is hit
 
 const ioHandler = async (req: NextApiRequest, res: NextApiResponseServerIo) => {
-    try {
-      console.log('Socket.io handler is called'); // Log when the handler is hit
-
-        // Kiểm tra xem server có socket.io chưa
-        if (!res.socket.server.io) {
-            const path = '/api/socket/io';
-            const httpServer: NetServer = res.socket.server as any;
-
-            // Khởi tạo Socket.io server
-            const io = new ServerIO(httpServer, {
-                path: path,
-                addTrailingSlash: false,
-                transports: ['polling', 'websocket']
-            });
-
-            res.socket.server.io = io; // Gán socket.io vào server
-        }
-
-        res.end(); // Kết thúc yêu cầu
-
-    } catch (error) {
-        // In lỗi vào console nếu có lỗi xảy ra
-        console.error('Error initializing socket.io:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    return res.status(200).json("test api")
 };
 
 export default ioHandler;
