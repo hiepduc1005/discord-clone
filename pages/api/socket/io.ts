705287611c5f463c69685e28a4173ae8081c9bac
hiepduc1005 <hiepngo1524@gmail.com>
2024-12-28 20:55:04 +0700

    update

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,34 +1,37 @@
-import {Server as NetServer} from 'http'
-import { Server as ServerIO} from "socket.io";
-import {NextApiResponseServerIo} from "@/type"
+import { Server as NetServer } from 'http';
+import { Server as ServerIO } from "socket.io";
+import { NextApiResponseServerIo } from "@/type";
 import { NextApiRequest } from 'next';
-
 
 export const config = {
     api: {
-      bodyParser: false, 
+        bodyParser: false, // Disable body parsing for Socket.io requests
     },
-  };
-  
+};
 
-const ioHandler = (req: NextApiRequest , res: NextApiResponseServerIo) => {
+const ioHandler = async (req: NextApiRequest, res: NextApiResponseServerIo) => {
+    try {
+        // Kiểm tra xem server có socket.io chưa
+        if (!res.socket.server.io) {
+            const path = '/api/socket/io';
+            const httpServer: NetServer = res.socket.server as any;
 
-    if(!res.socket.server.io){
-        const path = '/api/socket/io'
-        const httpServer: NetServer = res.socket.server as any;
-        const io = new ServerIO(httpServer,{
-            path: path,
-            addTrailingSlash: false,
-        })
+            // Khởi tạo Socket.io server
+            const io = new ServerIO(httpServer, {
+                path: path,
+                addTrailingSlash: false,
+            });
 
-        res.socket.server.io = io;
+            res.socket.server.io = io; // Gán socket.io vào server
+        }
 
-        
+        res.end(); // Kết thúc yêu cầu
+
+    } catch (error) {
+        // In lỗi vào console nếu có lỗi xảy ra
+        console.error('Error initializing socket.io:', error);
+        res.status(500).send('Internal Server Error');
     }
-    
-    res.end()
+};
 
-    
-}
-
-export default ioHandler
+export default ioHandler;
