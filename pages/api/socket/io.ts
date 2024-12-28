88e1dc014bc67e3a81c5b41f44bd98d6747c91ee
hiepduc1005 <hiepngo1524@gmail.com>
2024-12-28 22:38:51 +0700

    update

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -8,33 +8,31 @@ import { NextApiRequest } from 'next';
 //         bodyParser: false, // Disable body parsing for Socket.io requests
 //     },
 // };
-console.log('Socket.io init'); // Log when the handler is hit
-
 const ioHandler = async (req: NextApiRequest, res: NextApiResponseServerIo) => {
     try {
-      console.log('Socket.io handler is called'); // Log when the handler is hit
+    //   console.log('Socket.io handler is called'); // Log when the handler is hit
 
-        // Kiểm tra xem server có socket.io chưa
-        if (!res.socket.server.io) {
-            const path = '/api/socket/io';
-            console.log(path)
-            const httpServer: NetServer = res.socket.server as any;
+    //     // Kiểm tra xem server có socket.io chưa
+    //     if (!res.socket.server.io) {
+    //         const path = '/api/socket/io';
+    //         console.log(path)
+    //         const httpServer: NetServer = res.socket.server as any;
             
-            // Khởi tạo Socket.io server
-            const io = new ServerIO(httpServer, {
-                path: path,
-                addTrailingSlash: false,
+    //         // Khởi tạo Socket.io server
+    //         const io = new ServerIO(httpServer, {
+    //             path: path,
+    //             addTrailingSlash: false,
                 
-                transports: ['polling', 'websocket']
-            });
+    //             transports: ['polling', 'websocket']
+    //         });
             
-            console.log(io)
-            res.socket.server.io = io; // Gán socket.io vào server
+    //         console.log(io)
+    //         res.socket.server.io = io; // Gán socket.io vào server
 
-            io.on('error', (err) => {
-                console.log('Socket.IO error:', err.message, err.stack);
-              });
-        }
+    //         io.on('error', (err) => {
+    //             console.log('Socket.IO error:', err.message, err.stack);
+    //           });
+    //     }
 
         res.end(); // Kết thúc yêu cầu
 
